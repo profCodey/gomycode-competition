728d26e9be084d93e2df5dd1e76c6a8f19c372c4
profCodey <otunaiyademilade@gmail.com>
2024-03-22 19:01:37 +0100

    inital repo

---
+++ complete/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import UserProfile from './pages/userProfile'
+
+function App() {
+
+
+  return (
+    <>
+
+  <UserProfile />
+    </>
+  )
+}
+
+export default App
---
+++ empty/src/pages/userProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cards from '../components/userProfile/cards'
+import CardDetails from '../components/userProfile/cardDetails'
+
+const UserProfile = () => {
+  return (
+    <div>
+  
+          <Cards />
+          <CardDetails />
+    </div>
+  )
+}
+
+export default UserProfile
